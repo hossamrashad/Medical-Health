193327c990d7dd50cd33714b88593b61adb29e62
hossamrashad <bibo2010508@yahoo.com>
2021-06-06 16:47:37 +0200

    main-color

--- js/tools.js
+++ js/tools.js
@@ -9,3 +9,22 @@
 /*jslint browser: true */
 /*global window */
 
+
+// button scroll to top
+$(document).ready(function () {
+    "use strict";
+    $(window).scroll(function () {
+        var buttonScroll = $(".scroll-to-top");
+        if ($(window).scrollTop() >= 500) {
+            if (buttonScroll.is(":hidden")) {
+                buttonScroll.fadeIn(400);
+            }
+        } else {
+            buttonScroll.fadeOut(400);
+        }
+    });
+    $(".scroll-to-top").click(function (e) {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+    });
+});
