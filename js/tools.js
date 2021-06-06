3f745751f833ee06c5faba86b1e70d572b388c17
hossamrashad <bibo2010508@yahoo.com>
2021-06-06 03:08:03 +0200

    new

---
+++ js/tools.js
@@ -0,0 +1,25 @@
+/*jslint plusplus: true, evil: true */
+// jslint plusplus: true for error for ++
+// evil: true for error document.write
+
+/*global console , alert , prompt , $ , document, write, ADSAFE, report, jslint, bitwise, node*/
+
+/*property charAt, slicexs*/
+
+/*jslint browser: true */
+/*global window */
+
+// $(function () {
+//   "use strict";
+//   // smoothly scroll
+
+//   $(".navbar-nav li a").click(function () {
+//     $("html, body").animate(
+//       {
+//         screenTop: $("#" + $(this).data("scroll")).offset().top
+//       },
+//       1000
+//     );
+//     console.log("#" + $(this).data("scroll"));
+//   });
+// });
